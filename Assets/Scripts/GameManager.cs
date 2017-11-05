9a621d293a5947d66ff68d83ac0d2bfed6a17976
Vast <Vastlee@gmail.com>
2017-11-04 19:19:16 -0800

    Added Initial Project, MonoSingleton, and Example

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,3 @@
+﻿public class GameManager : MonoSingleton<GameManager> {
+
+}
---
+++ Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : Component {
+	private static T instance = null;
+    private static bool isQuitting = false;
+	public static T Instance {
+		get {
+			if(instance == null && !isQuitting)
+                FindOrCreateInstance();
+			return instance;
+		}
+	}
+
+    static private void FindOrCreateInstance() {
+        T[] instanceArray = FindObjectsOfType<T>();
+        if(instanceArray.Length == 0) {
+            GameObject singleton = new GameObject(string.Empty);
+            instance = singleton.AddComponent<T>();
+            DontDestroyOnLoad(singleton);
+        } else if(instanceArray.Length == 1) {
+            instance = instanceArray[0];
+            DontDestroyOnLoad(instance);
+        } else if(instanceArray.Length > 1) {
+            Debug.LogError("Multiple instances of a singleton exists.");
+            Debug.Break();
+        }
+    }
+
+    private void OnApplicationQuit() {
+        isQuitting = true;
+    }
+}
